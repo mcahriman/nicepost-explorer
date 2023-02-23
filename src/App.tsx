d09a47eb3cd55c807660bf89c519dd7d1081b6b0
Yurii Borysov <mcahriman@gmail.com>
2023-02-23 19:50:18 +0200

    Test Movies sending

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,61 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import axios from "axios";
 
 import MovieList from "components/MoviesList";
+import AddMovie from "components/AddMovie";
+import { movie } from "components/types";
 
-const App = () => {
-  const [movies, setMovies] = useState([]);
+type Props = { moviesEndpoint: string };
+
+const App = ({moviesEndpoint} : Props) => {
+  const [movies, setMovies] = useState<movie[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
-  useEffect(() => {
-    fetchMoviesHandler();
-  }, []);
 
-  const fetchMoviesHandler = async () => {
+
+
+  const fetchMoviesHandler = useCallback(async () => {
     setError("");
     try {
       setLoading(true);
-      const result = await axios.get("https://swapi.dev/api/films/");
-      setMovies(
-        result.data.results.map((movie: any) => {
-          return {
-            id: movie.episode_id,
-            title: movie.title,
-            releaseDate: movie.release_date,
-            openingText: movie.opening_crawl,
-          };
-        })
-      );
+      const result = await axios.get(moviesEndpoint);
+      console.log(result);
+      const loadedMovies: movie[] = [];
+      for (const key in result.data) {
+        loadedMovies.push( {...result.data[key], id: key} );
+      }
+      setMovies(loadedMovies);
       setLoading(false);
     } catch (error) {
       setError(error instanceof Error ? error.message : "Something went wrong");
       setLoading(false);
     }
-  };
+  }, []);
+
+  async function addMovieHandler(movie: movie) {
+    setLoading(true);
+
+    const response = await fetch(moviesEndpoint, {
+      method: "POST",
+      body: JSON.stringify(movie),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    setMovies((prevMovies) => [...prevMovies, { ...movie, id: data.name }]);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
   return (
     <div>
+      <AddMovie onAddMovie={addMovieHandler} />
       {loading && <p>Loading...</p>}
       {!loading && <MovieList movies={movies} />}
       {error && <p>{error}</p>}
