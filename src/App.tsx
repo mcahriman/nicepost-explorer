ae7553a0f7c9ee81cd890954f63ebb36951a2460
Yurii Borysov <mcahriman@gmail.com>
2023-02-26 17:16:44 +0200

    Add Instagram

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import PasteInput from "components/PasteInput";
 import "./App.css";
-import expandExample from "examples/expandExample";
+import { expandExample, expandExampleInstagram } from "examples/expandExample";
 import ReactCodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 import PasteResult from "components/PasteResult";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClipboardCheck, faClipboard, faPoll, faWarning } from "@fortawesome/free-solid-svg-icons";
-
+import {
+  faClipboardCheck,
+  faClipboard,
+  faPoll,
+  faWarning,
+} from "@fortawesome/free-solid-svg-icons";
 
 type Props = { converterApi: string };
 
@@ -15,8 +19,8 @@ const App = ({ converterApi }: Props) => {
   const [copied, setCopied] = useState(false);
   const [pastedJson, setPastedJson] = useState<string | null>(null);
 
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText(expandExample).then(() => {
+  const copyToClipboard = (data: string) => {
+    navigator.clipboard.writeText(data).then(() => {
       setCopied(true);
       setTimeout(() => {
         setCopied(false);
@@ -26,34 +30,71 @@ const App = ({ converterApi }: Props) => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1><FontAwesomeIcon icon={faPoll}/>Poor Man S***post Explorer&trade;</h1>
+        <h1>
+          <FontAwesomeIcon icon={faPoll} />
+          Poor Man S***post Explorer&trade;
+        </h1>
       </header>
 
       <main>
         <nav>
           <h3>1. Copy code below and run it in console on m.facebook.com</h3>
-          <h3><FontAwesomeIcon icon={faWarning}/>Works in Firefox only</h3>
+          <h3>
+            <FontAwesomeIcon icon={faWarning} />
+            Works in Firefox only
+          </h3>
 
-          <button
-            className="copyButton"
-            onClick={copyToClipboard}
-            title="Copy to clipboard"
-          >
-            {copied ?  <FontAwesomeIcon icon={faClipboardCheck}  /> : <FontAwesomeIcon icon={faClipboard}  />}
-          </button>
+          <div className="codeMirrorExample">
+            <h4>m.facebook.com</h4>
+            <button
+              className="copyButton"
+              onClick={() => {copyToClipboard(expandExample)}}
+              title="Copy to clipboard"
+            >
+              {copied ? (
+                <FontAwesomeIcon icon={faClipboardCheck} />
+              ) : (
+                <FontAwesomeIcon icon={faClipboard} />
+              )}
+            </button>
 
-          <ReactCodeMirror
-            className="codeContainer"
-            value={expandExample}
-            extensions={[javascript({})]}
-            height="30rem"
-            width="100%"
-          />
+            <ReactCodeMirror
+              className="codeContainer"
+              value={expandExample}
+              extensions={[javascript({})]}
+              height="30rem"
+              width="100%"
+            />
+          </div>
+
+          <div className="codeMirrorExample">
+            <h4>instagram web</h4>
+            <button
+              className="copyButton"
+              onClick={() => {copyToClipboard(expandExampleInstagram)}}
+              title="Copy to clipboard"
+            >
+              {copied ? (
+                <FontAwesomeIcon icon={faClipboardCheck} />
+              ) : (
+                <FontAwesomeIcon icon={faClipboard} />
+              )}
+            </button>
+
+            <ReactCodeMirror
+              className="codeContainer"
+              value={expandExampleInstagram}
+              extensions={[javascript({})]}
+              height="30rem"
+              width="100%"
+            />
+          </div>
+
           <h3>2. Paste the result into the text area below</h3>
-          <PasteInput onPasteJson={setPastedJson}/>
+          <PasteInput onPasteJson={setPastedJson} />
         </nav>
         <div className="mainContent">
-          <PasteResult data={pastedJson}/>
+          <PasteResult data={pastedJson} />
         </div>
       </main>
     </div>
---
+++ src/examples/expandExample-instagram.source.js
@@ -0,0 +1,56 @@
+
+function expandAll() {
+    var prevlength = 0;
+// data-visualcompletion="loading-state" 
+    const generateTextArea = () => {
+        results = $$("ul._a9ym").map( (element) => {
+            profileLink = element.querySelector("span a")
+            comment = element.querySelector("span._aade")
+          	commentlink = element.querySelector("div._aacl._aacn._aacu._aacy._aad6 > a");
+            
+          	commentId = commentlink?.href || Math.random
+          
+            return [
+              profileLink.innerText,
+              comment.innerText,
+              profileLink.getAttribute("href"),
+              commentId,
+            ]
+        });
+        textarea = document.createElement('textarea');
+        textarea.value = JSON.stringify(results);
+        document.body.appendChild(textarea);
+        textarea.style = "position: fixed; width: 80%; height:80%; top:10%; left:10%; z-index: 999"
+        textarea.focus();
+        textarea.select();
+        console.log(results);
+
+    }
+  
+    function expand() {
+        button = $("div._ab8w._ab94._ab99._ab9h._ab9m._ab9p._abcj._abcm button._abl-");
+        loading = $("[data-visualcompletion=loading-state]")
+        if (!button && !loading) {
+            console.log("No more links to click, exiting")
+            generateTextArea();
+            return;
+        }
+        if (!loading) {
+          console.log("Not loading can do stuff")
+          if (document.body.innerText.length === prevlength) {
+            console.log("lenght did not update after loading, exciting!")
+            generateTextArea();
+            return;
+          } else {
+            prevlength = document.body.innerText.length;
+          }
+          console.log("Clicking load more again")
+          button.click();
+        }
+        setTimeout(expand, 2000);
+      }
+    expand();
+  }
+  
+  expandAll();
+  
